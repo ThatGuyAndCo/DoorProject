3fd3f7691f1a0b7461eb000af4a8ec86c19db6e9
ThatGuyAndCo <jollypolly889@gmail.com>
2017-10-13 08:51:19 +0100

    Merge commit

--- DoorUnityProject/CrystalDoorProject/Assets/Scripts/ActivateCrystal.cs
+++ DoorUnityProject/CrystalDoorProject/Assets/Scripts/ActivateCrystal.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class ActivateCrystal : MonoBehaviour {
 
@@ -8,19 +9,26 @@ public class ActivateCrystal : MonoBehaviour {
 	private Animator m_anim;
 
 	[SerializeField]
-	private AudioClip[] m_audioClips;
+	private AudioMixerGroup[] m_mixers;
+
+	private AudioSource m_audio;
 
 	// Use this for initialization
 	void Start () {
 		m_anim = GetComponent<Animator> ();
+		m_audio = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space) && m_anim.GetBool ("CrystalOpen")) {
 			m_anim.SetBool ("CrystalOpen", false);
+			m_audio.outputAudioMixerGroup = m_mixers [0];
+			m_audio.Play ();
 		} else if(Input.GetKeyDown (KeyCode.Space)){
 			m_anim.SetBool ("CrystalOpen", true);
+			m_audio.outputAudioMixerGroup = m_mixers [1];
+			m_audio.Play ();
 		}
 	}
 }
