6ee3147603146e19a197318dd4d88a97ca59ac1f
ThatGuyAndCo <jollypolly889@gmail.com>
2017-10-27 11:26:03 +0100

    Added new interaction sounds and made them binaural

--- DoorUnityProject/CrystalDoorProject/Assets/Scripts/ActivateCrystal.cs
+++ DoorUnityProject/CrystalDoorProject/Assets/Scripts/ActivateCrystal.cs
@@ -11,6 +11,9 @@ public class ActivateCrystal : MonoBehaviour {
 	[SerializeField]
 	private AudioMixerGroup[] m_mixers;
 
+	[SerializeField]
+	private AudioClip[] m_interactionClips;
+
 	private AudioSource m_audio;
 
 	// Use this for initialization
@@ -22,13 +25,19 @@ public class ActivateCrystal : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown (KeyCode.Space) && m_anim.GetBool ("CrystalOpen")) {
-			m_anim.SetBool ("CrystalOpen", false);
-			m_audio.outputAudioMixerGroup = m_mixers [0];
+			Invoke ("CloseCrystal", 0.5f);
+			m_audio.outputAudioMixerGroup = m_mixers [1];
+			m_audio.clip = m_interactionClips [1];
 			m_audio.Play ();
 		} else if(Input.GetKeyDown (KeyCode.Space)){
 			m_anim.SetBool ("CrystalOpen", true);
-			m_audio.outputAudioMixerGroup = m_mixers [1];
+			m_audio.outputAudioMixerGroup = m_mixers [0];
+			m_audio.clip = m_interactionClips [0];
 			m_audio.Play ();
 		}
+	}
+
+	void CloseCrystal(){
+		m_anim.SetBool ("CrystalOpen", false);
 	}
 }
